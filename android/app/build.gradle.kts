013cd67e6ffa90e3432eb8dfcd0d76b2479eedd5
osamafrahat <osamaelmesay1@gmail.com>
2026-08-14 02:18:32 +0300

    Fix Android build: update compileSdk to 36, remove unused dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.passcoder"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
